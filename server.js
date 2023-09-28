ea992ed823802b8eb90fc5daf9ffeacf44f49497
Sou <samelsonjohn@protonmail.com>
2023-09-27 23:46:24 -0400

    added cors

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+
 require('dotenv').config();
+
 const githubRoutes = require('./routes/githubRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use('/githubContributions', githubRoutes);
 
 app.listen(PORT, () => {
